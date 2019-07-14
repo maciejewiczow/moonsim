296499a32d0427c2c97acaccb58aa7281d437e2c
maciejewiczow <37718435+maciejewiczow@users.noreply.github.com>
2019-07-14 23:01:36 +0200

    Revert "Project setup (#1)" (#2)
    This reverts commit cbcc8265fcf9294851460c1ff757ccf9275105ab.

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
-
-app.use('/', express.static('dist'))
-app.listen(port, () => console.info('Listening on port ' + port))
--- src/typings/assets.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.dae' {
-    const link: string
-    export default link
-}
-
-declare module '*.jpg' {
-    const link: string
-    export default link
-}
