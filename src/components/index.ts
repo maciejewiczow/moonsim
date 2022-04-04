56a8e4d804309aae67aca02aa7411fd34702ee83
Mafciejewicz <melonik0.9846@gmail.com>
2022-04-04 22:00:55 +0200

    Add phase slider

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
-export { MasterLayout } from './MasterLayout'
-export { Background } from './Background'
-export { MoonCanvas } from './MoonCanvas'
-export { MoonStatus } from './MoonStatus'
-export { PhaseSlider } from './PhaseSlider'
-export { Settings } from './Settings'
+export { MasterLayout } from './MasterLayout';
+export { MoonScene as MoonCanvas } from './MoonCanvas';
+export { MoonStatus } from './MoonStatus';
+export { PhaseSlider } from './PhaseSlider';
+export { Settings } from './Settings';
